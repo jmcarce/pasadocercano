d062a14173208e5fb27733fd1c0c0641a181df75
jmcarce <jmcarce@outlook.es>
2021-01-10 16:40:52 +0000

    funcional postgis arranque coordinado down code 0

---
+++ app/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Esperando a que arranque PostgreSQL..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL iniciado"
+fi
+
+python3 manage.py flush --no-input
+python3 manage.py migrate
+
+exec "$@"
