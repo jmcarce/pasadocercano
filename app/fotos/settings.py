7eee3320f1f17f03c2f0171a596e203337bac8e6
jmcarce <jmcarce@outlook.es>
2021-01-12 18:48:59 +0000

    final - mediafiles produccion funcional

--- app/fotos/settings.py
+++ app/fotos/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +114,6 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
